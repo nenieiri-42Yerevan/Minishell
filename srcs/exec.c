f482e8b816784b792578db309818d3026baa00bd
Volodya Ismailyan <nenie_iri@mail.ru>
2022-04-19 20:08:28 +0400

    Heredoc execution

--- srcs/exec.c
+++ srcs/exec.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 16:18:39 by vismaily          #+#    #+#             */
-/*   Updated: 2022/04/19 18:15:54 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/04/19 20:05:34 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,23 @@
 void	child(char *path, t_command *command, char **envp)
 {
 	int		i;
+	int		heredoc[2];
 
 	if (command->std_in != 0)
 		dup2(command->std_in, 0);
 	if (command->std_out != 1)
 		dup2(command->std_out, 1);
+	if (ft_strncmp(command->oper, "<<", 3) == 0 && command->heredoc != 0)
+	{
+		i = pipe(heredoc);
+		if (i == -1)
+			exit(1);
+		ft_putstr_fd(command->heredoc, heredoc[1]);
+		ft_putstr_fd("\n", heredoc[1]);
+		dup2(heredoc[0], 0);
+		close(heredoc[0]);
+		close(heredoc[1]);
+	}
 	i = execve(path, command->args, envp);
 	if (i == -1)
 		printf("Minishell$ command not found: %s\n", command->args[0]);
