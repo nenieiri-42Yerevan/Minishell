3c91ba669ba623530b356c9b07661a390f966fd1
Volodya Ismailyan <nenie_iri@mail.ru>
2022-04-27 18:23:43 +0400

    Change pipes and redirections priorities

--- srcs/dups.c
+++ srcs/dups.c
@@ -6,28 +6,14 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 20:39:41 by vismaily          #+#    #+#             */
-/*   Updated: 2022/04/26 16:05:54 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/04/27 18:18:17 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	dups(t_command *tmp)
+static void	dups_outs(t_command *tmp)
 {
-	if (tmp->pipe_in != 0)
-	{
-		tmp->std_in_dup1 = dup(0);
-		dup2(tmp->pipe_in, 0);
-		close(tmp->pipe_in);
-		if (tmp->std_in != 0)
-			close(tmp->std_in);
-	}
-	else if (tmp->std_in != 0)
-	{
-		tmp->std_in_dup1 = dup(0);
-		dup2(tmp->std_in, 0);
-		close(tmp->std_in);
-	}
 	if (tmp->std_out != 1)
 	{
 		tmp->std_out_dup1 = dup(1);
@@ -42,4 +28,23 @@ void	dups(t_command *tmp)
 		dup2(tmp->pipe_out, 1);
 		close(tmp->pipe_out);
 	}
+}
+
+void	dups(t_command *tmp)
+{
+	if (tmp->std_in != 0)
+	{
+		tmp->std_in_dup1 = dup(0);
+		dup2(tmp->std_in, 0);
+		close(tmp->std_in);
+		if (tmp->pipe_in != 0)
+			close(tmp->pipe_in);
+	}
+	else if (tmp->pipe_in != 0)
+	{
+		tmp->std_in_dup1 = dup(0);
+		dup2(tmp->pipe_in, 0);
+		close(tmp->pipe_in);
+	}
+	dups_outs(tmp);
 }
